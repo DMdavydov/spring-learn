a4f182c270ab700200fa0fa68f6e4fecedfd0d6b
DMdavydov <dmitry_davydov1@epam.com>
2020-10-19 20:55:56 +0300

    thymeleaf bootstrap + aop

--- src/main/java/com/ddavydov/config/SecurityConfig.java
+++ src/main/java/com/ddavydov/config/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.ddavydov.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
